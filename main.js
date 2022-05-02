8d18a5f1a6adfc94c745ac8a6db5070c37902ead
unknown <adrijana.jovicic@gmail.com>
2022-05-02 17:49:54 +0200

    created options

--- main.js
+++ main.js
@@ -9,6 +9,11 @@ let booksViewBtn = document.querySelector('#books-view-btn');
 let newBookViewBtn = document.querySelector('#new-book-view-btn');
 let editDeleteViewBtn = document.querySelector('#edit-delete-btn');
 
+//FORMS
+// Add new book
+let genreSelect = document.querySelector('#genre-select');
+let publisherSelect = document.querySelector('#publisher-select');
+
 // LISTENERS
 booksViewBtn.addEventListener('click', displayBooksView);
 newBookViewBtn.addEventListener('click', displayNewBookView);
@@ -28,6 +33,8 @@ function displayNewBookView(e) {
     if (e) {
         e.preventDefault()
     }
+    createGenreOptions();
+    createPublisherOptions();
     editDeleteView.style.display = "none";
     booksView.style.display = "none";
     newBookView.style.display = "block";
@@ -40,4 +47,25 @@ function displayEditDeleteBookView(e) {
     booksView.style.display = "none";
     newBookView.style.display = "none";
     editDeleteView.style.display = "block";
+}
+
+// Options
+function createGenreOptions() {
+    let text = '';
+    allGenre.forEach(genre => {
+        text += `
+            <option value="${genre}">${genre}</option>
+        `.trim()
+    })
+    genreSelect.innerHTML = text;
+}
+
+function createPublisherOptions() {
+    let text = '';
+    allPublisher.forEach(pub => {
+        text += `
+            <option value="${pub}">${pub}</option>
+        `.trim()
+    })
+    publisherSelect.innerHTML = text;
 }
