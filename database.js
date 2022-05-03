b5cc00c612fd401df1f176ebce242a4c76db262b
unknown <adrijana.jovicic@gmail.com>
2022-05-03 11:47:13 +0200

    add new book

--- database.js
+++ database.js
@@ -2,3 +2,7 @@ let db = [];
 
 let allGenre = ['Fantasy', 'Sci-Fi', 'Mystery', 'Thriller', 'Romance', 'Westerns', 'Dystopian', 'Contemporary'];
 let allPublisher = ['Pub1', 'Pub2', 'Pub3', 'Pub4'];
+
+if (localStorage.db) {
+    db = JSON.parse(localStorage.db);
+}
--- main.js
+++ main.js
@@ -1,16 +1,23 @@
+window.addEventListener('beforeunload', save);
 // VIEWS
 let booksView = document.querySelector('#book-view');
 let editDeleteView = document.querySelector('#edit-delete-view');
 let newBookView = document.querySelector('#new-book-view');
+
+let booksTbody = document.querySelector('tbody');
 
 // BUTTONS
 // Navbar
 let booksViewBtn = document.querySelector('#books-view-btn');
 let newBookViewBtn = document.querySelector('#new-book-view-btn');
 let editDeleteViewBtn = document.querySelector('#edit-delete-btn');
+let saveBtn = document.querySelector('#save-btn');
 
 //FORMS
 // Add new book
+let inputName = document.querySelector('[name="book"]');
+let inputDate = document.querySelector('#date');
+// Options
 let genreSelect = document.querySelector('#genre-select');
 let publisherSelect = document.querySelector('#publisher-select');
 
@@ -18,8 +25,13 @@ let publisherSelect = document.querySelector('#publisher-select');
 booksViewBtn.addEventListener('click', displayBooksView);
 newBookViewBtn.addEventListener('click', displayNewBookView);
 editDeleteViewBtn.addEventListener('click', displayEditDeleteBookView);
+saveBtn.addEventListener('click', saveNewBook);
 
+function save() {
+    localStorage.db = JSON.stringify(db);
+}
 
+// Displays
 function displayBooksView(e) {
     if (e) {
         e.preventDefault()
@@ -68,4 +80,59 @@ function createPublisherOptions() {
         `.trim()
     })
     publisherSelect.innerHTML = text;
+}
+
+// Create Books Table
+function createBookTable(currentDb) {    
+    console.log(currentDb);
+    if (!currentDb) {
+        currentDb = db;
+    }
+    let text = '';
+    currentDb.forEach(book => {
+        text += `
+        <tr>
+            <td>${book.id}</td>
+            <td>${book.name}</td>
+            <td>${book.ganre}</td>
+            <td>${book.publisher}</td>
+            <td>${book.date}</td>
+         </tr>
+        `.trim()
+    })
+    booksTbody.innerHTML = text;
+    
+}
+
+//Save new book
+function saveNewBook() {
+    let newBook = {
+        id: generateId(),
+        name: inputName.value,
+        ganre: genreSelect.value,
+        publisher: publisherSelect.value,
+        date: inputDate.value
+    }
+    db.push(newBook);
+    console.log(db);
+    
+    createBookTable(db);
+    displayBooksView();
+    
+
+}
+// create ID
+function generateId() {
+    let randomId;
+    let unique = false;
+    while (!unique) {
+        unique = true;
+        randomId = Math.floor(Math.random() * 100000);
+        db.forEach(book => {
+            if (parseInt(book.id) === randomId) {
+                unique = false;
+            }
+        })
+    }
+    return randomId.toString();
 }
